dd5ab9317e2a2d1b85015be3ae50d1043590c016
NelsonNV <nelsonluisnv2@gmail.com>
2025-01-02 17:21:51 -0300

    chore: consulta extra inecesaria

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,6 @@ import { inicializarUI, renderContenido } from './ui.js';
 
 // Inicializar aplicación al cargar el DOM
 document.addEventListener('DOMContentLoaded', async () => {
-    await listarArchivos();
     inicializarUI(); // Configurar eventos de UI
     renderContenido(); // Render inicial de la lista
 });
